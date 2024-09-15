3314fd6a5b92b7332a42fd89cf59b3d701d78cfa
Sahilyadav24 <sahilrao2424@gmail.com>
2024-09-16 01:26:51 +0530

    Added source files and compiled class files

---
+++ src/EmplyeePayrollSystem/Employee.java
@@ -0,0 +1,27 @@
+package EmplyeePayrollSystem;
+//we have used access modifiers and abstraction
+
+abstract public class Employee {
+    private String name;
+    private int id;
+    public Employee(String name, int id){//we made parametrized constructor whenever constructor calls then name and id will assign to these variables
+        this.id=id;
+        this.name=name;
+
+    }
+    public String getName(){//we have used encapsulation here because we have made name and id private anybody can not asses them outside the class
+        //to achive some security so anybody can not acces it and change it directly we have only provided get method to just watch it and not change it
+        return name;
+    }
+    public int getid(){
+        return id;
+    }
+
+    public abstract double calculatesallary();//here we made abstract method that's why we made whole class abstract
+
+    @Override
+    public String toString(){ //we have override this toString method because it is inbuild method of java we we have to use it accoring to our needs
+        return"Employee[name = "+name+ ",id = "+id+ " salary = "+calculatesallary()+"]";
+    }
+
+}
---
+++ src/EmplyeePayrollSystem/main.java
@@ -0,0 +1,20 @@
+package EmplyeePayrollSystem;
+
+public class main {
+    public static void main(String[] args) {
+        PayRollSystem payRollSystem=new PayRollSystem();
+        FullTimeEmployee emp1 = new FullTimeEmployee("Rohan", 01, 30000);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Rohit", 23,40,1200);
+
+        payRollSystem.addEmployee(emp1);
+        payRollSystem.addEmployee(emp2);
+        System.out.println("Initial Employee Details ");
+        payRollSystem.displayEmployee();
+
+        System.out.println("Removing Employee");
+        payRollSystem.removeEmployee(23);
+
+        System.out.println("Remaining Employee Details");
+        payRollSystem.displayEmployee();
+    }
+}
